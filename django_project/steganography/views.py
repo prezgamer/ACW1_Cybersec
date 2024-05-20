ff7c5629ea0cde053609453da711484301dce107
prezgamer <50046059+prezgamer@users.noreply.github.com>
2024-05-20 10:40:25 +0800

    Reapply "Update for test site"
    This reverts commit 7427b12f5141a1fca0d1554208c6eb2b50faee6a.

--- django_project/steganography/views.py
+++ django_project/steganography/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ImageUploadForm
 
 # Create your views here.
 def home(request):
-    return render(request, 'steganography/home.html')
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('success')
+    else:
+        form = ImageUploadForm()
+    return render(request, 'steganography/home.html', {'form': form})
+
+def success(request):
+    return render(request, 'steganography/success.html')
