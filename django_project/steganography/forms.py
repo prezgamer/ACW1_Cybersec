4268967067ebeac5039a1cfb8f9b8b6d9d0d2a2f
prezgamer <50046059+prezgamer@users.noreply.github.com>
2024-06-10 23:17:18 +0800

    Add file add to encode_message

--- django_project/steganography/forms.py
+++ django_project/steganography/forms.py
@@ -7,7 +7,7 @@ class StegoImageForm(forms.ModelForm):
     num_lsbs = forms.IntegerField(min_value=0, max_value=7, label="Number of LSBs")
     class Meta:
         model = StegoImage
-        fields = ['original_image', 'message','num_lsbs']
+        fields = ['message_file','original_image', 'message','num_lsbs']
 
 class StegoDecodeForm(forms.Form):
     stego_image = forms.ImageField(label="Stego Image")
